844b091a4cc5966dcaf1f205f2fc55940901b6ee
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2023-10-02 13:24:17 -0400

    Re-add validations and quick-deploy for Apex on push pipelines.

--- deploy_metadata_sf.py
+++ deploy_metadata_sf.py
@@ -20,6 +20,7 @@ def parse_args():
         manifest - path to the package.xml file
         wait - number of minutes to wait for command to complete
         environment - Salesforce environment URL
+        pipeline - pipeline type (push or merge_request_event)
         log - deploy log where the output of this script is being written to
             python ./deploy_metadata_sfdx.py --args | tee -a deploy_log.txt
             -a flag required to append to file during run-time
@@ -32,6 +33,7 @@ def parse_args():
     parser.add_argument('-w', '--wait', default=33)
     parser.add_argument('-e', '--environment')
     parser.add_argument('-l', '--log', default='deploy_log.txt')
+    parser.add_argument('-p', '--pipeline', default='push')
     parser.add_argument('-v', '--validate', default=False, action='store_true')
     parser.add_argument('-d', '--debug', default=False, action='store_true')
     args = parser.parse_args()
@@ -86,46 +88,45 @@ def quick_deploy(deploy_id, wait):
     run_command(command)
 
 
-def main(testclasses, manifest, wait, environment, log, validate, debug):
+def main(testclasses, manifest, wait, environment, log, pipeline, validate, debug):
     """
         Main function to deploy metadata to Salesforce.
     """
-    # omit test level and dry-run when validating non-apex packages
-    if validate and testclasses == 'not a test':
-        command = 'sf project deploy start --dry-run'
-    # validate with tests on apex packages
-    elif validate:
-        command = f'sf project deploy validate -l RunSpecifiedTests -t {testclasses}'
-    # deploy  with tests on apex packages
-    elif testclasses != 'not a test':
-        command = f'sf project deploy start -l RunSpecifiedTests -t {testclasses}'
-    # omit test level when deploying non-apex packages
-    else:
-        command = "sf project deploy start"
-    command += f' -x {manifest} -w {wait} --verbose'
+    # Define the command
+    command = (f'{f"sf project deploy validate -l RunSpecifiedTests -t {testclasses}" if (validate and testclasses != "not a test") else "sf project deploy start"}'
+                f'{" --dry-run" if (validate and testclasses == "not a test") else ""}'
+                f' -x {manifest} -w {wait} --verbose')
     logging.info(command)
+
+    if validate and testclasses == 'not a test' and pipeline == 'push':
+        logging.info('Not running validation deployment without test classes.')
+        return
 
     if debug:
         return
 
-    # Create deploy log
+    # Create deploy log to avoid any threading errors
     with open(log, 'w', encoding='utf-8'):
         pass
 
     # Create result dictionary to store deploy_id
     result = {}
 
-    # create and start threads
+    # create and start read thread to run in parallel with deployment
     read_thread = threading.Thread(target=create_sf_link, args=(environment, log, result))
     # set read thread to daemon so it automatically terminates when
     # the main program ends
     # ex: if package.xml is empty, no ID is created and the thread will continue to run
     read_thread.daemon = True
     read_thread.start()
+    # start the deployment
     run_command(command)
+    # run quick-deploy after a validation on a push pipeline (apex)
+    if validate and pipeline == 'push':
+        quick_deploy(result.get('deploy_id'), wait)
 
 
 if __name__ == '__main__':
     inputs = parse_args()
     main(inputs.tests, inputs.manifest, inputs.wait, inputs.environment,
-         inputs.log, inputs.validate, inputs.debug)
+         inputs.log, inputs.pipeline, inputs.validate, inputs.debug)
