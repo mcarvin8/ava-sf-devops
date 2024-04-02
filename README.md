322a3f91816e5cb13e57d19cbd41d1a65f454616
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2024-04-02 09:01:40 -0400

    add prod backfill script

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Each Salesforce org has its own long-running Git branch.
 The pipeline is divided into the following stages:
 
 - The `build` stage builds a Docker image for the org if the `Dockerfile` has been modified. This Docker image is pushed to the GitLab repository's Container Registry.
+- The `backfill` stage pushes commits from `main` branch backwards to `fullqa` and `develop` branches. This is required for branching strategies where developers create branches from `main`, but have to merge their branches into other long running branches (`develop` and `fullqa` in this template).
+    - This job requires a project access token configured for the GitLab repository - https://docs.gitlab.com/ee/user/project/settings/project_access_tokens.html
+    - Add 3 CI/CD variables to the repo called `BOT_NAME`, `BOT_USER_NAME` and `PROJECT_TOKEN`. The `BOT_NAME` should be the name of the project access token/bot account. The `BOT_USER_NAME` will be the user name for the bot account created when making the project access token (ex: `project_{project_id}_bot_{random_string}`). The `PROJECT_TOKEN` will be the passphrase generated after making the token.
+    - The Docker image requires a version of Git which can skip pipelines during a push (`git push -o ci.skip`).
 - The `validate` stage contains jobs for each org. When a merge request is opened against one of the org branches, it will validate the changes in the org.
     - This has been confirmed on GitLab Merge Request Pipelines (standard pipeline - defaults to this when there are merge conflicts) and Merged Results Pipelines (when there are no conflicts and this setting is enabled in the repo)
     - If you are working on GitLab v16.6, adjust the variable $COMMIT_MSG to use $CI_MERGE_REQUEST_DESCRIPTION to ensure MR pipelines with merge conflicts parse the tests and package in the MR description.
---
+++ scripts/bash/merge_main_into_sbx_cicd.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+# CI/CD Variables to Set in Repo:
+# $BOT_NAME = name of the project access token/bot user - ex: JARVIS
+# $BOT_USER_NAME = user name for the project access token bot user - ex: project_{project_id}_bot_{random_string}
+# $PROJECT_TOKEN = project access token passphrase
+
+function accept_incoming_changes_merge() {
+    # check if there are any merge conflicts
+    if [[ $(git status | grep "Unmerged paths:") ]]; then
+        echo "Merge conflicts detected. Resolving automatically by accepting all incoming changes on the below files:"
+        # loop through all files with merge conflicts and accept incoming changes
+        while IFS= read -r -d '' file; do
+            echo "$file"
+            git checkout --theirs "$file"
+            git add "$file"
+        done < <(git diff --name-only --diff-filter=U -z)
+        git commit -m "Merge remote-tracking branch 'origin/main' into $1"
+    else
+        echo "No merge conflicts detected. Continuing without resolving conflicts."
+    fi
+}
+
+# Must fetch before checking out fullqa and develop branches
+# Configure bot user name and bot user email address for this project access token
+# Bot Email Address template: project_{project_id}_bot_{random_string}@noreply.{Gitlab.config.gitlab.host}
+git reset --hard
+git fetch
+git config user.name "${BOT_NAME}"
+git config user.email "${BOT_USER_NAME}@noreply.${CI_SERVER_HOST}"
+
+# Update the sandbox branches with changes from production
+for branch_name in fullqa develop
+do
+    git checkout $branch_name
+    git pull --ff
+    # Merge changes from main branch, ignoring merge conflict errors
+    git merge --no-ff origin/main || true
+    accept_incoming_changes_merge "$branch_name"
+    # Push changes to remote, skipping CI pipeline
+    git push "https://${BOT_NAME}:${PROJECT_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}" -o ci.skip
+done
+
+# Cleanup, switch back to original branch and delete local branches
+git checkout main
+git branch -D fullqa
+git branch -D develop
