94680490638a26c841d178e2210557bd3213228d
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2025-03-11 21:47:03 -0400

    Update create_package.sh

--- scripts/bash/create_package.sh
+++ scripts/bash/create_package.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+# Create package by combining incremental package and commit message package
 # $COMMIT_MSG should vary when validating from a merge request
 # $COMMIT_MSG: $CI_MERGE_REQUEST_DESCRIPTION for validation
 # $COMMIT_MSG: $CI_COMMIT_MESSAGE for deployment
-# $DEPLOY_PACKAGE needs to be re-defined as "package.xml" in the .gitlab-ci.yml to use this script
-# Only use this script for validate and deploy to allow additional metadata. Fully rely on sfdx-git-delta for destruction packages.
 
 # Define a function to build package.xml from commit message
 build_package_from_commit() {
