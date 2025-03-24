be710cc5a0e53a16670acea39ab1cbf32a452317
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2025-03-24 10:44:07 -0400

    remove comments

--- scripts/bash/create_destroy_package.sh
+++ scripts/bash/create_destroy_package.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# This is an alternate destructive pipeline script which takes in a package list (created via "convert_package_to_list.sh")
-# and creates the destructive packages before a destructive deployment.
-# This could be used in a web-based pipeline that isn't triggered by changes pushed to a git branch.
-# You could copy a package created by Workbench and convert the package to this format.
 set -e
 # Create directory if it don't exist
 mkdir -p "destructiveChanges"
