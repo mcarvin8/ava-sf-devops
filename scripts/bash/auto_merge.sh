ae801d969e86a8bcc1ddc4687bad8712d4ff3f05
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2025-04-15 09:01:58 -0400

    add auto-merge jobs for each org branch

--- scripts/bash/auto_merge.sh
+++ scripts/bash/auto_merge.sh
@@ -3,14 +3,6 @@
 # if there are merge conflicts, the source branch file version will be used
 set -e
 
-# Must fetch before checking out branches
-git fetch -q
-git config user.name "${PAT_NAME}"
-git config user.email "${PAT_USER_NAME}@noreply.${CI_SERVER_HOST}"
-
-git checkout -q $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
-git pull --ff -q
-
 # Attempt auto-merge preferring source content when possible
 git merge --no-ff -X theirs --no-commit origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME || true
 
@@ -24,10 +16,3 @@ git ls-files -u | cut -f2 | sort -u | while read -r file; do
 done
 
 git commit -m "Merge remote-tracking branch 'origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME' into $CI_MERGE_REQUEST_TARGET_BRANCH_NAME. Triggered by: $GITLAB_USER_NAME"
-
-# Push changes to remote
-git push "https://${PAT_NAME}:${PAT_VALUE}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
-
-# Cleanup, switch back to the SHA that triggered this pipeline and delete local branches
-git -c advice.detachedHead=false checkout -q $CI_COMMIT_SHORT_SHA
-git branch -D $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
