de06a4eb112faf3f00a53675fe2c93eb9fe9adc9
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2025-04-17 16:34:26 -0400

    include package list in auto merge commit

--- scripts/bash/auto_merge.sh
+++ scripts/bash/auto_merge.sh
@@ -15,5 +15,17 @@ git ls-files -u | cut -f2 | sort -u | while read -r file; do
     git add "$file" 2>/dev/null || true
 done
 
-git commit -m "Merge remote-tracking branch 'origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME' into $CI_MERGE_REQUEST_TARGET_BRANCH_NAME. Triggered by: $GITLAB_USER_NAME"
+# Extract package info from merge request description
+PACKAGE_LIST=$(echo "$CI_MERGE_REQUEST_DESCRIPTION" | sed -n '/<Package>/,/<\/Package>/p')
+
+# Create commit message
+COMMIT_MSG="Merge remote-tracking branch 'origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME' into $CI_MERGE_REQUEST_TARGET_BRANCH_NAME. Triggered by: $GITLAB_USER_NAME"
+if [[ -n "$PACKAGE_LIST" ]]; then
+    COMMIT_MSG+="
+
+Packages list in this merge request:
+$PACKAGE_LIST"
+fi
+
+git commit -m "$COMMIT_MSG"
 git push "https://${PAT_NAME}:${PAT_VALUE}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
